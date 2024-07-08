aace41fd438465e1d50db2b06940f5ee6d580605
Bastian <bastian.rojash@usm.cl>
2024-07-08 19:30:23 -0400

    GP-3
    Redireccion

---
+++ lib/pages/administrador/registrar_usuario_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegistrarUsuarioPage extends StatefulWidget {
+  const RegistrarUsuarioPage({super.key});
+
+  @override
+  State<RegistrarUsuarioPage> createState() => _RegistrarUsuarioPageState();
+}
+
+class _RegistrarUsuarioPageState extends State<RegistrarUsuarioPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
