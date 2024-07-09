0a660d0ebb9b48fe3bfd194ea852e13e3ce1906b
Chofo's Machine <diegovalenciavives@gmail.com>
2024-07-09 17:06:44 -0400

    GP-21 preparación tests

--- lib/pages/dev/dev_page.dart
+++ lib/pages/dev/dev_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:grupaso_pedidos/pages/administrador/administrador_page.dart';
 import 'package:grupaso_pedidos/pages/cocina_page.dart';
 import 'package:grupaso_pedidos/pages/crearPedido_page.dart';
-import 'package:grupaso_pedidos/pages/dev/example_page.dart';
 import 'package:grupaso_pedidos/pages/login.dart';
 import 'package:grupaso_pedidos/pages/caja_page.dart';
 import 'package:grupaso_pedidos/pages/verPedido.dart';
@@ -10,7 +9,6 @@ import 'package:grupaso_pedidos/pages/verPedido.dart';
 // recuerda recargar ----->
 var pages = [
   VerPedidoPage(),
-  ExamplePage(),
   CrearPedidoPage(),
   LoginPage(),
   CajaPage(),
